d7c78ede8e1cfe1a03df554e0f02ba7d20f75fa6
LavishGorilla97 <26310269+zhengli6@users.noreply.github.com>
2019-07-29 14:51:27 -0500

    Add files via upload

--- sort_kernel.c
+++ sort_kernel.c
@@ -22,11 +22,28 @@ typedef struct
 parameters *data;
 
 /* swap function*/
-static void swap(int* a, int* b)
+void swap_pair(int* a, int* b)
 {
     int t = *a;
     *a = *b;
     *b = t;
+}
+
+/*partition function*/
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low-1);
+    for (int j = low; j<high; j++)
+    {
+        if (arr[j]<= pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i+1], &arr[high]);
+    return (i+1);
 }
 
 // Function executed by kernel thread
